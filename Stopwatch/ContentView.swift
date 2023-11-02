6c495b923f2d8277c27edaa50fb2663d9a45ef30
Sophie Haber <SHaber@lcs.on.ca>
2023-11-02 09:51:08 -0400

    Got the basic structure of the first circular buttom completed.

--- Stopwatch/ContentView.swift
+++ Stopwatch/ContentView.swift
@@ -13,16 +13,42 @@ struct ContentView: View {
         
         ZStack {
             
-            // First layer
+        // First layer
             Color.black
                 .ignoresSafeArea()
             
-            //Second layer (rest of interface)
+        //Second layer (rest of interface)
             VStack {
                 
                 Text("00:09.96")
                     .font(Font.system(size: 90,weight: .thin))
                     .foregroundColor(.white)
+                
+        //Create a circular buttom
+                ZStack {
+                    
+        //First layer
+                    Circle()
+                        .foregroundColor(.gray)
+                    .frame(width: 100)
+                    
+        //Second layer
+                    Circle()
+                        .foregroundColor(.black)
+                    .frame(width: 93)
+                    
+        //Third layer
+                    Circle()
+                        .foregroundColor(.gray)
+                    .frame(width: 89)
+                    
+        //Forth layer
+                    Text("Reset")
+                        .foregroundColor(.white)
+                        .font(.title2)
+                    
+                    
+                }
                 
             }
             .padding()
