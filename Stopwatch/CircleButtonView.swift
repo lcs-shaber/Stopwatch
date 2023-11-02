35619fd00b29b4b79b68c161374cd7c6b388fa23
Sophie Haber <SHaber@lcs.on.ca>
2023-11-02 10:02:12 -0400

    Created CircleButtonView as a helper to make ContentView shorter.

---
+++ Stopwatch/CircleButtonView.swift
@@ -0,0 +1,18 @@
+//
+//  CircleButtonView.swift
+//  Stopwatch
+//
+//  Created by Sophie Haber on 02.11.23.
+//
+
+import SwiftUI
+
+struct CircleButtonView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CircleButtonView()
+}
