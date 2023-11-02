f8b02cd3afa27414ce7fbe3dd04c49799e387c22
Sophie Haber <SHaber@lcs.on.ca>
2023-11-02 08:47:11 -0400

    Initial Commit

---
+++ Stopwatch/StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StopwatchApp.swift
+//  Stopwatch
+//
+//  Created by Sophie Haber on 02.11.23.
+//
+
+import SwiftUI
+
+@main
+struct StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
