02b1d009d7ce139bcaa1637a3229b866d95e026e
Sophie Haber <SHaber@lcs.on.ca>
2023-11-05 14:45:57 -0500

    Added a simple list and made the position of user interface elements mostly match the goal.

--- Stopwatch/ContentView.swift
+++ Stopwatch/ContentView.swift
@@ -21,6 +21,8 @@ struct ContentView: View {
         //Second layer (rest of interface)
             VStack {
                 
+                Spacer()
+                
                 Text("00:09.96")
                     .font(Font.system(size: 90,weight: .thin))
                     .foregroundColor(.white)
@@ -41,6 +43,17 @@ struct ContentView: View {
                         labelColor: .green
                     )
                 }
+                
+        //List of times
+                List{
+                    Text("1")
+                    Text("2")
+                    Text("3")
+                    Text("4")
+                    Text("5")
+                }
+        //Set the amount of verticle height we want this list to take up
+                .frame(height: 300)
                 
             }
             .padding()
